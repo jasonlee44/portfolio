af90d012c35895bdad73aa0aa337df37973374f7
Jason Lee <jasonlee@Jasons-MacBook-Air-5.local>
2024-01-05 13:00:12 -0500

    Updated resume pdf

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -5,7 +5,7 @@ export default function Resume() {
         <>
             <div id="resume" class="block text-white pt-[80px]">
                 <h2 class="text-4xl font-bold text-blue-500 text-center">Resume</h2>
-                <object data="/src/assets/Jason_Resume.pdf" type="application/pdf" class="mx-auto" width="70%" height="1000px"></object>
+                <object data="./src/assets/Jason_Resume.pdf" type="application/pdf" class="mx-auto" width="70%" height="1000px"></object>
             </div>
         </>
     )
